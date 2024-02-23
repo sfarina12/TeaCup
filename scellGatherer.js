bf5daac69a1f236f46dbb10dc7bd9bad8c72faa1
Simone Farina <35815576+sfarina12@users.noreply.github.com>
2024-02-23 12:06:48 +0100

    added small fixes

--- scellGatherer.js
+++ scellGatherer.js
@@ -1,6 +1,5 @@
 var dnd_class = "wizard";
 $(document).ready(function(){
-
   $.ajax({
     url: "https://www.dnd5eapi.co/api/classes/"+dnd_class+"/spells", 
     success: function(result){
@@ -23,7 +22,23 @@ $(document).ready(function(){
         });
       })
     }});
-
 });
+
+var open_bottom = false;
+$("body").on('click', '#expandFilter', function() {
+  if(!open_bottom) {
+    $("#bottom_navigator").attr("style","height: 75%;align-items: flex-start;")
+    $("#bottom_navigator text").addClass("expanded_bottom_content")
+    $("#expandFilter").addClass("expanded_bottom_content")
+    $("#expandFilter").attr("style","height: calc(9% - 18px);")
+  }
+  if(open_bottom) {
+    $("#bottom_navigator").attr("style","");
+    $("#bottom_navigator text").removeClass("expanded_bottom_content")
+    $("#expandFilter").removeClass("expanded_bottom_content")
+    $("#expandFilter").attr("style","")
+  }
+  open_bottom = !open_bottom;
+})
 
 
