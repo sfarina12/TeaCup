e10d6011c4f895a1dd4c96636a10fd31ba5c3403
Simone Farina <35815576+sfarina12@users.noreply.github.com>
2024-02-24 15:14:23 +0100

    partially added level filter

--- scellGatherer.js
+++ scellGatherer.js
@@ -9,7 +9,17 @@ $(document).ready(function(){
     success: function(result){
       
       spell_types.push("All")
-      $("#spell_types_list").append("<li>All</li>")
+
+      thisll1 = $("<section class='active' tipo='All'>All"+htmllevel()+"</section>")
+      $("#spell_types_list").prepend(thisll1)
+
+      $($(thisll1).children()[0]).on("scroll", function(){
+        thisll1.trigger("custom-scroll");
+      })
+
+      $("#spell_types_list").append(thisll1)
+      $("#spell_types_list").append("<section style='color:white'>1</section>")
+      $("#spell_types_list").append("<section style='color:white'>1</section>")
 
       result.results.forEach(function(k,v) {
         $.ajax({
@@ -18,7 +28,13 @@ $(document).ready(function(){
               type = result.school.name
               if(!spell_types.includes(type)) {
                 spell_types.push(type)
-                $("#spell_types_list").append("<li>"+type+"</li>")
+
+                thisll = $("<section tipo='"+type+"'>"+type+htmllevel()+"</section>")
+                $("#spell_types_list").prepend(thisll)
+
+                $($(thisll).children()[0]).on("scroll", function(){
+                  thisll.trigger("custom-scroll");
+                })
               }
 
               icon = "https://www.dndbeyond.com/content/1-0-2639-0/skins/waterdeep/images/spell-schools/35/"+result.school.index+".png"
@@ -34,19 +50,24 @@ $(document).ready(function(){
                       '</div>'+
                   '</div>')
               }
-          }
+            }
         });
       })
+      
     }});
 });
+
+function htmllevel() {
+  return '<div class="spell_types_level"><section>-</section><section>1</section><section>2</section><section>3</section><section>4</section><section>5</section><section>6</section><section>7</section><section>8</section><section>9</section><section style="color:white">.</section><section style="color:white">.</section><section style="color:white">.</section><section style="color:white">.</section><section style="color:white">.</section><section style="color:white">.</section><section style="color:white">.</section></div>';
+}
 
 var open_bottom = false;
 $("body").on('click', '#expandFilter', function() {
   openclose_filter();
 })
 
-$("body").on('click', '#spell_types_list li', function() {
-  filtered_type = $(this).html()
+$("body").on('click', '#spell_types_list section', function() {
+  filtered_type = $(this).attr("tipo")
   act_filter = filtered_type
   $("#lista").empty()
   $("#act_filter").html(filtered_type)
@@ -69,8 +90,8 @@ $("body").on('click', '#spell_types_list li', function() {
 
 function openclose_filter() {
   if(!open_bottom) {
-    $("#bottom_navigator").attr("style","height: 63%;")
-    $("#selected_filter").attr("style","height: 13%;")
+    $("#bottom_navigator").attr("style","height: 30%;")
+    $("#selected_filter").attr("style","height: 36%;")
     $("#expandFilter img").attr("src","https://img.icons8.com/fluency-systems-filled/48/ff7300/collapse-arrow.png")
   }
   if(open_bottom) {
@@ -108,4 +129,30 @@ $(document).on('touchend',"#touchSupport",function(e){
   } else {
     $("#content_brawser").attr("style","transition: height cubic-bezier(0, 0, 0, 0.99) 0.3s,background cubic-bezier(0, 0, 0, 0.99) 0.3s;");
   }
+})
+
+$("#spell_types_list").on("scroll",function(){
+  ll = $("#spell_types_list").children()
+  ll.each(function (index, ele) {
+    if (Math.abs(ele.getBoundingClientRect().top - $("#spell_types_list")[0].getBoundingClientRect().top) < 15) {
+      $(ele).addClass("active")
+      $($(ele).children()[0]).addClass("visible")
+      
+    } else {
+      $(ele).removeClass("active")
+      $($(ele).children()[0]).removeClass("visible")
+    }
+  });
+})
+
+$("body").on("custom-scroll",function(){
+  ll = $(".spell_types_level").children()
+  ll.each(function (index, ele) {
+    if (Math.abs(ele.getBoundingClientRect().left - $(ele).parent()[0].getBoundingClientRect().left) < 10) {
+      if($($($(ele).parent()).parent()).hasClass("active"))
+        $(ele).addClass("active_")
+    } else {
+      $(ele).removeClass("active_")
+    }
+  });
 })
